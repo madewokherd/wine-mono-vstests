97d370513170ca344bd1285cc0d02498539b4c07
Esme Povirk <esme@codeweavers.com>
2022-11-26 14:24:23 -0600

    ApartmentState doesn't actually change when set.

--- mixedapartmentstate/mixedapartmentstate.cpp
+++ mixedapartmentstate/mixedapartmentstate.cpp
@@ -5,7 +5,15 @@ using namespace System::Threading;
 
 int main(array<System::String ^> ^args)
 {
+    if (Thread::CurrentThread->ApartmentState != ApartmentState::MTA) {
+        return 1;
+    }
+
     Thread::CurrentThread->ApartmentState = ApartmentState::STA;
+
+    if (Thread::CurrentThread->ApartmentState != ApartmentState::MTA) {
+        return 2;
+    }
 
     return 0;
 }
